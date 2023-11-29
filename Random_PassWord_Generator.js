0434b415055fd1771f55679ab8a06c39ba096bf9
midhun-r <134675201+midhun-r@users.noreply.github.com>
2023-11-29 22:40:46 +0530

    Update Random_PassWord_Generator.js

--- Random_PassWord_Generator.js
+++ Random_PassWord_Generator.js
@@ -1,1 +1,14 @@
-// Javascript code here.. 
+function generatePassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  return password;
+}
+
+const newPassword = generatePassword(15);
+console.log(newPassword);
